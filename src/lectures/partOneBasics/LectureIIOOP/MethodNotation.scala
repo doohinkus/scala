11c15d0eaa443c02cbda5d2303599b55349a8456
Valen Longfeather <valenlongfeather@Valens-MacBook-Pro.local>
2021-03-12 12:27:44 -0800

    add some more operator notation info

--- src/lectures/partOneBasics/LectureIIOOP/MethodNotation.scala
+++ src/lectures/partOneBasics/LectureIIOOP/MethodNotation.scala
@@ -5,7 +5,8 @@ object MethodNotation extends App{
     def likes(movie:String):Boolean = movie == favoriteMovie
     def hangsWith(person: Person):String = s"${this.name} hangs with ${person.name}"
     def ^(person: Person): String = s"${this.name} and ${person.name } wear ^ hats because special characters can be methods in Scala!!!"
-
+    def isAlive():String = s"Person is alive!"
+    def apply(): String =s"Hey, my name is $name and I love $favoriteMovie."
   }
   val mary = new Person("Mary", "Shining")
   println(mary.likes("Shining"))
@@ -17,7 +18,26 @@ object MethodNotation extends App{
   println(mary.hangsWith(jim))
   println(mary hangsWith tom)
 
+  // ALL operators are METHODS in Scala
   println(mary.^(jim))
   println(mary ^ tom)
+
+  // prefix notation
+  // unary operators ARE methods
+  // only works with + - ~ !
+  // must use space after : in def
+  // def unary_! : String = s"$name is great!"
+  val x = -1
+  val y = 1.unary_-
+
+
+  //postfix notation only works with NO PARAM method
+  println(mary isAlive)
+  println(mary.isAlive)
+
+
+  // apply
+  println(mary.apply)
+  println(jim())
 
 }
